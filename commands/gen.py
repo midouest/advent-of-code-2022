861ea7f16b56f2386880f012aff986ba68078b14
Robin <midouesterner@gmail.com>
2022-12-09 21:45:03 -0600

    gen command can re-download inputs

--- commands/gen.py
+++ commands/gen.py
@@ -29,10 +29,21 @@ class GenCommand(Command):
 
     def add_arguments(self, parser: ArgumentParser):
         parser.add_argument(
-            "day", type=int, choices=range(1, 26), help="The day to generate"
+            "day",
+            type=int,
+            choices=range(1, 26),
+            help="The day to generate",
+        )
+        parser.add_argument(
+            "--input",
+            action="store_true",
+            help="Only download the puzzle input",
         )
 
-    def exec(self, day: int):
+    def exec(self, day: int, input: bool):
         puzzle_input = puzzle.fetch_input(day)
         puzzle.write_input(day, puzzle_input)
+        if input:
+            return
+
         puzzle.write_solution(day, template)
