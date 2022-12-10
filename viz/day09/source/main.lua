ec96e641180600233723d9981fea15b01f2ecfcb
Robin <midouesterner@gmail.com>
2022-12-09 18:00:34 -0600

    menu options for part 1 and part 2

--- viz/day09/source/main.lua
+++ viz/day09/source/main.lua
@@ -3,6 +3,7 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/crank"
+import "puzzle"
 
 local pd<const> = playdate
 local gfx<const> = pd.graphics
@@ -14,8 +15,11 @@ local screenHTile<const> = screenHPx / tilePx
 
 local minRopeLength<const> = 2
 local maxRopeLength<const> = 10
-local startX<const> = 0
-local startY<const> = 0
+local gridSize<const> = 1000
+local startX<const> = gridSize // 2
+local startY<const> = gridSize // 2
+local minPos<const> = 0
+local maxPos<const> = gridSize - 1
 
 local visited
 local rope
@@ -24,10 +28,9 @@ local ropeLength
 local isDirty
 local viewX
 local viewY
+local puzzle
 
-local function coordKey2(x, y)
-    return tostring(math.tointeger(x)) .. "," .. tostring(math.tointeger(y))
-end
+local function coordKey2(x, y) return gridSize * x + y end
 local function coordKey1(coord) return coordKey2(coord[1], coord[2]) end
 
 local function sgn(n)
@@ -100,38 +103,113 @@ function setUpGame()
     markVisited()
 end
 
+local function moveLeft()
+    local head<const> = rope[1]
+    if head[1] - 1 < minPos then return end
+    moveRope(1, -1)
+    if head[1] < viewX + 1 then viewX = viewX - 1 end
+end
+local function moveRight()
+    local head<const> = rope[1]
+    if head[1] + 1 > maxPos then return end
+    moveRope(1, 1)
+    if head[1] > viewX + screenWTile - 2 then viewX = viewX + 1 end
+end
+local function moveUp()
+    local head<const> = rope[1]
+    if head[2] - 1 < minPos then return end
+    moveRope(2, -1)
+    if head[2] < viewY + 1 then viewY = viewY - 1 end
+end
+local function moveDown()
+    local head<const> = rope[1]
+    if head[2] + 1 > maxPos then return end
+    moveRope(2, 1)
+    if head[2] > viewY + screenHTile - 2 then viewY = viewY + 1 end
+end
+
+local function stopPuzzle()
+    if puzzle == nil then return end
+    puzzle:stop()
+    puzzle = nil
+end
+
+local function updatePuzzle()
+    if puzzle == nil then return end
+    if not puzzle:isRunning() then
+        stopPuzzle()
+        return
+    end
+    puzzle:run()
+end
+
+local function startPart1()
+    stopPuzzle()
+    setUpGame()
+    puzzle = Puzzle({R = moveRight, L = moveLeft, U = moveUp, D = moveDown})
+end
+
+local function startPart2()
+    startPart1()
+    for _ = ropeLength, maxRopeLength do lengthenRope() end
+end
+
 function setUpMenu()
     local menu<const> = pd.getSystemMenu()
+    menu:addMenuItem("Part 1", startPart1)
+    menu:addMenuItem("Part 2", startPart2)
     menu:addMenuItem("Reset", setUpGame)
 end
 
 setUpMenu()
 setUpGame()
 
-local function moveLeft()
-    moveRope(1, -1)
-    if rope[1][1] < viewX + 1 then viewX = viewX - 1 end
-end
-local function moveRight()
-    moveRope(1, 1)
-    if rope[1][1] > viewX + screenWTile - 2 then viewX = viewX + 1 end
-end
-local function moveUp()
-    moveRope(2, -1)
-    if rope[1][2] < viewY + 1 then viewY = viewY - 1 end
-end
-local function moveDown()
-    moveRope(2, 1)
-    if rope[1][2] > viewY + screenHTile - 2 then viewY = viewY + 1 end
+local keyRepeatTimer
+
+local function stopKeyRepeat()
+    if keyRepeatTimer ~= nil then keyRepeatTimer:remove() end
 end
 
-function pd.leftButtonDown() moveLeft() end
-function pd.rightButtonDown() moveRight() end
-function pd.upButtonDown() moveUp() end
-function pd.downButtonDown() moveDown() end
+local function startKeyRepeat(func)
+    stopKeyRepeat()
+    keyRepeatTimer = pd.timer.keyRepeatTimer(func)
+end
 
-function pd.BButtonDown() shortenRope() end
-function pd.AButtonDown() lengthenRope() end
+function pd.leftButtonDown()
+    if puzzle ~= nil then return end
+    startKeyRepeat(moveLeft)
+end
+function pd.leftButtonUp() stopKeyRepeat() end
+
+function pd.rightButtonDown()
+    if puzzle ~= nil then return end
+    startKeyRepeat(moveRight)
+end
+function pd.rightButtonUp() stopKeyRepeat() end
+
+function pd.upButtonDown()
+    if puzzle ~= nil then return end
+    startKeyRepeat(moveUp)
+end
+function pd.upButtonUp() stopKeyRepeat() end
+
+function pd.downButtonDown()
+    if puzzle ~= nil then return end
+    startKeyRepeat(moveDown)
+end
+function pd.downButtonUp() stopKeyRepeat() end
+
+function pd.BButtonDown()
+    if puzzle ~= nil then
+        stopPuzzle()
+    else
+        shortenRope()
+    end
+end
+function pd.AButtonDown()
+    if puzzle ~= nil then return end
+    lengthenRope()
+end
 
 local function tileToScreen(x, y)
     return (x - viewX) * tilePx, (y - viewY) * tilePx
@@ -159,22 +237,26 @@ local function redraw()
         local y<const> = viewY + yOff
         for xOff = 0, screenWTile - 1 do
             local x<const> = viewX + xOff
-            local key<const> = coordKey2(x, y)
-            local tile = "."
-            local i = ropeCoords[key]
-            if i ~= nil then
-                tile = ropeTile(i)
-            elseif x == startX and y == startY then
-                tile = "s"
-            elseif visited[key] ~= nil then
-                tile = "#"
+            if x >= minPos and x <= maxPos and y >= minPos and y <= maxPos then
+                local key<const> = coordKey2(x, y)
+                local tile = "."
+                local i = ropeCoords[key]
+                if i ~= nil then
+                    tile = ropeTile(i)
+                elseif x == startX and y == startY then
+                    tile = "s"
+                elseif visited[key] ~= nil then
+                    tile = "#"
+                end
+                drawTile(tile, x, y)
             end
-            drawTile(tile, x, y)
         end
     end
 end
 
 function pd.update()
+    updatePuzzle()
+
     if isDirty then
         redraw()
         isDirty = false
