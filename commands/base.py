ed9468a8a5a60770f352530a4085c8a59bd8875e
Robin <midouesterner@gmail.com>
2022-12-18 11:17:24 -0600

    command base class is abstract

--- commands/base.py
+++ commands/base.py
@@ -1,12 +1,15 @@
 from argparse import ArgumentParser
+from abc import ABC, abstractmethod
 
 
-class Command:
+class Command(ABC):
     name: str
     description: str
 
+    @abstractmethod
     def add_arguments(self, parser: ArgumentParser):
         raise NotImplementedError()
 
+    @abstractmethod
     def exec(self, *args, **kwargs):
         raise NotImplementedError()
