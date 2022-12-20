c11a5058acdc67b8c070088af46f26a38fe1da02
Robin <midouesterner@gmail.com>
2022-12-20 06:42:45 -0600

    search returns visited, allows initializing distances

--- util/search.py
+++ util/search.py
@@ -8,6 +8,27 @@ from collections import deque, defaultdict
 Node = TypeVar("Node", bound=Hashable)
 
 
+class Path(Generic[Node]):
+    def __init__(self, nodes: list[Node], visited: set[Node]):
+        self.nodes = nodes
+        self.visited = visited
+
+    def __len__(self):
+        return len(self.nodes)
+
+    def __iter__(self):
+        return iter(self.nodes)
+
+    def __reversed__(self):
+        return reversed(self.nodes)
+
+    def __contains__(self, item):
+        return item in self.nodes
+
+    def __getitem__(self, key):
+        return self.nodes[key]
+
+
 class Search(Generic[Node], ABC):
     @abstractmethod
     def initial(self) -> Iterable[Node]:
@@ -33,7 +54,7 @@ class Search(Generic[Node], ABC):
         path.reverse()
         return path
 
-    def search(self, take: Callable[[deque[Node]], Node]) -> list[Node] | None:
+    def search(self, take: Callable[[deque[Node]], Node]) -> Path[Node] | None:
         frontier: deque[Node] = deque(self.initial())
         visited: set[Node] = set(frontier)
         parents: dict[Node, Node] = {}
@@ -41,7 +62,7 @@ class Search(Generic[Node], ABC):
         while frontier:
             current = take(frontier)
             if self.goal(current):
-                return self.reconstruct_path(current, parents)
+                return Path(self.reconstruct_path(current, parents), visited)
 
             for neighbor in self.neighbors(current):
                 if neighbor not in visited:
@@ -51,14 +72,17 @@ class Search(Generic[Node], ABC):
 
         return None
 
-    def bfs(self) -> list[Node] | None:
+    def bfs(self) -> Path[Node] | None:
         return self.search(lambda frontier: frontier.popleft())
 
-    def dfs(self) -> list[Node] | None:
+    def dfs(self) -> Path[Node] | None:
         return self.search(lambda frontier: frontier.pop())
 
 
 class AStarSearch(Search[Node]):
+    def initial_distance(self, node: Node) -> int:
+        return 0
+
     @abstractmethod
     def distance(self, current: Node, neighbor: Node) -> int:
         raise NotImplementedError()
@@ -67,13 +91,13 @@ class AStarSearch(Search[Node]):
     def heuristic(self, node: Node) -> int:
         raise NotImplementedError()
 
-    def astar(self) -> list[Node] | None:
+    def astar(self) -> Path[Node] | None:
         initial = self.initial()
 
         g_score: dict[Node, float] = defaultdict(lambda: math.inf)
         f_score: dict[Node, float] = defaultdict(lambda: math.inf)
         for node in initial:
-            g_score[node] = 0
+            g_score[node] = self.initial_distance(node)
             f_score[node] = self.heuristic(node)
 
         open_set: set[Node] = set(initial)
@@ -85,7 +109,9 @@ class AStarSearch(Search[Node]):
             open_set.remove(current)
 
             if self.goal(current):
-                return self.reconstruct_path(current, parents)
+                return Path(
+                    self.reconstruct_path(current, parents), set(g_score.keys())
+                )
 
             for neighbor in self.neighbors(current):
                 tentative_g_score = g_score[current] + self.distance(current, neighbor)
@@ -106,5 +132,5 @@ class DjikstraSearch(AStarSearch[Node]):
     def heuristic(self, node: Node) -> int:
         return 0
 
-    def djikstra(self) -> list[Node]:
+    def djikstra(self) -> Path[Node]:
         return self.astar()
