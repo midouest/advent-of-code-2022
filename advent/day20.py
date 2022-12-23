8b2ef0e1654b178a88965b9459d91d3a191e40e4
Robin <midouesterner@gmail.com>
2022-12-23 10:15:43 -0600

    day 20 part 2 solution

--- advent/day20.py
+++ advent/day20.py
@@ -1,15 +1,14 @@
 from util.prelude import *
 
 
-def debug(file):
-    print(list(map(itemgetter(1), file)))
+def parse_input(input: str):
+    return map(int, input.strip().split("\n"))
 
 
-def mix(input: str) -> list[int]:
-    file = list(enumerate(map(int, input.strip().split("\n"))))
-    size = len(file)
+def mix(file: list[tuple[int, int]]):
+    original_size = len(file)
 
-    for order in range(size):
+    for order in range(original_size):
         i, value = next(
             (index, value)
             for index, (original, value) in enumerate(file)
@@ -18,27 +17,40 @@ def mix(input: str) -> list[int]:
         if value == 0:
             continue
         del file[i]
+        new_size = len(file)
 
         j = i + value
-        k = j
-        if j <= 0:
-            k = (j - 1) % len(file)
-        elif j >= len(file):
-            k = j % len(file)
+        k = j % new_size
         file.insert(k, (order, value))
 
+    return file
+
+
+def mix_n(file: Iterable[int], n=1):
+    file = list(enumerate(file))
+    for _ in range(n):
+        file = mix(file)
     return [v for _, v in file]
 
 
-def part1(input: str):
-    file = mix(input)
+def decrypt(file: Iterable[int]):
+    file = [n * 811589153 for n in file]
+    return mix_n(file, 10)
+
+
+def sum_grove_coordinates(file: list[int]):
     zero = file.index(0)
     size = len(file)
-    return sum(file[(zero + i) % size] for i in [1000, 2000, 3000])
+    coords = [file[(zero + i) % size] for i in [1000, 2000, 3000]]
+    return sum(coords)
+
+
+def part1(input: str):
+    return sum_grove_coordinates(mix_n(parse_input(input)))
 
 
 def part2(input: str):
-    raise NotImplementedError()
+    return sum_grove_coordinates(decrypt(parse_input(input)))
 
 
 example = """1
@@ -56,4 +68,4 @@ def test_part1():
 
 
 def test_part2():
-    assert part2(example) == 0
+    assert part2(example) == 1623178306
